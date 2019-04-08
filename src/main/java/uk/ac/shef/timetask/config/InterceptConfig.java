0f495682db36d56e0f3247c778aa3edd7abf5a5d
zmyouknow <137332024@qq.com>
2019-04-08 17:35:26 +0800

    加入登录拦截器

---
+++ src/main/java/uk/ac/shef/timetask/config/InterceptConfig.java
@@ -0,0 +1,16 @@
+package uk.ac.shef.timetask.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import uk.ac.shef.timetask.intercept.LoginInterceptor;
+
+public class InterceptConfig extends WebMvcConfigurerAdapter {
+    @Autowired
+    private LoginInterceptor loginInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor).addPathPatterns("/**").excludePathPatterns("/js/**", "/css/**", "/img/**");
+    }
+}
